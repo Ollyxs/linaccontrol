53dcf8da5c3a6b351447a80f3c8845ca558c414b
Lucas Ollarce <luk5ollarce@gmail.com>
2024-11-06 02:11:34 -0300

    feat(service): allow updating test results

--- app/services/test_results_service.py
+++ app/services/test_results_service.py
@@ -1,6 +1,9 @@
 from sqlmodel import select
 from app.models import TestResults
-from app.schemas.test_results_schema import TestResultsCreateModel
+from app.schemas.test_results_schema import (
+    TestResultsCreateModel,
+    TestResultsUpdateModel,
+)
 from sqlmodel.ext.asyncio.session import AsyncSession
 from typing import List
 from uuid import UUID
@@ -43,11 +46,33 @@ class TestResultsService:
         await session.commit()
         return new_test_results
 
-    # async def create_test_result(
-    #     self, test_result_data: TestResultsCreateModel, session: AsyncSession
-    # ):
-    #     test_result_data_dict = test_result_data.model_dump()
-    #     new_test_result = TestResults(**test_result_data_dict)
-    #     session.add(new_test_result)
-    #     await session.commit()
-    #     return new_test_result
+    async def update_test_results(
+        self,
+        result_uid: UUID,
+        test_results_data: List[TestResultsUpdateModel],
+        session: AsyncSession,
+    ):
+        existing_test_results = await self.get_test_results_by_result_uid(
+            result_uid, session
+        )
+        existing_test_results_dict = {tr.test_uid: tr for tr in existing_test_results}
+
+        for test_result_data in test_results_data:
+            test_result_data_dict = test_result_data.model_dump()
+            test_uid = test_result_data_dict["test_uid"]
+            if test_uid in existing_test_results_dict:
+                existing_test_result = existing_test_results_dict[test_uid]
+                existing_test_result.result = test_result_data_dict["result"]
+            else:
+                new_test_results = TestResults(
+                    test_uid=test_result_data_dict["test_uid"],
+                    result=test_result_data_dict["result"],
+                    results_uid=result_uid,
+                )
+                session.add(new_test_results)
+
+        await session.commit()
+        test_results_data_updated = await self.get_test_results_by_result_uid(
+            result_uid, session
+        )
+        return test_results_data_updated

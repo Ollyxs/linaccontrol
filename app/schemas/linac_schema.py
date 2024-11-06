258a09a379d4106b0327a876087930f510b0fe1e
Lucas Ollarce <luk5ollarce@gmail.com>
2024-11-06 17:46:53 -0300

    feat(schema): add creation date for linac

--- app/schemas/linac_schema.py
+++ app/schemas/linac_schema.py
@@ -1,4 +1,5 @@
 from sqlmodel import SQLModel
+from datetime import date
 from uuid import UUID
 from typing import Optional
 
@@ -7,19 +8,21 @@ class LinacModel(SQLModel):
     uid: UUID
     name: str
     location: str
-    # image: Optional[bytes] = None
+    image: Optional[bytes] = None
+    create_at: date
     is_active: bool
 
 
 class LinacCreateModel(SQLModel):
     name: str
     location: str
-    # image: Optional[bytes] = None
+    image: Optional[bytes] = None
+    create_at: Optional[date] = None
     is_active: bool
 
 
 class LinacUpdateModel(SQLModel):
     name: Optional[str] = None
     location: Optional[str] = None
-    # image: Optional[bytes] = None
+    image: Optional[bytes] = None
     is_active: Optional[bool] = None
